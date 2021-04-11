27cdafae72098a32222cc987f06172d32ffa9283
Unmuerto <unmuerto@bk.ru>
2021-04-11 21:05:55 +0300

    added service, made changes in main component, added styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
+import {
+  ApiTableResponse,
+  ApiVariablesResponse,
+  TableData,
+  Variables,
+} from './interfaces';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [ApiService],
 })
-export class AppComponent {
-  title = 'coreGearsTest';
+export class AppComponent implements OnInit {
+  title = 'CoreGears Test Task';
+  variables: Variables[] = [];
+  tableData: TableData[] = [];
+  columnSum: Partial<Record<keyof TableData, number>> = {};
+  isTableLoaded = false;
+
+  constructor(private _apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this._fetchVariablesData();
+    this._fetchTableData();
+  }
+
+  updateValues(): void {
+    this._apiService.updateData(this.variables).subscribe(() => {
+      this._fetchVariablesData();
+      this._fetchTableData();
+    });
+  }
+
+  private _fetchVariablesData(): void {
+    this._apiService.fetchData().subscribe((data: ApiVariablesResponse) => {
+      this.variables = data.result;
+    });
+  }
+
+  private _fetchTableData(): void {
+    this.isTableLoaded = false;
+    this._apiService.fetchTable().subscribe((data: ApiTableResponse) => {
+      this.tableData = data.result.map((record) => ({
+        ...record,
+        sum: Object.values(record)
+          .slice(1)
+          .reduce((acc, value) => acc + value),
+      }));
+
+      this.tableData.forEach((record) => {
+        Object.keys(record)
+          .slice(1)
+          .forEach((key) => {
+            this.columnSum[key as keyof TableData] = this.columnSum[
+              key as keyof TableData
+            ]
+              ? this.columnSum[key as keyof TableData]! +
+                (record[key as keyof TableData] as number)
+              : (record[key as keyof TableData] as number);
+          });
+      });
+      this.isTableLoaded = true;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ApiService } from './api.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  providers: [ApiService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
